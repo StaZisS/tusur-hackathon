5759dcf62b2a71489ff4216d6e7166f653339661
Gordey Dovydenko <inostaziss@gmail.com>
2024-04-18 19:06:37 +0700

    fix url for keycloak

--- src/main/java/org/hits/backend/hackathon_tusur/HackathonTusurApplication.java
+++ src/main/java/org/hits/backend/hackathon_tusur/HackathonTusurApplication.java
@@ -14,3 +14,26 @@ public class HackathonTusurApplication {
     }
 
 }
+
+/*
+https://stackoverflow.com/questions/38337895/globally-disable-https-keycloak
+  keycloak:
+    image: quay.io/keycloak/keycloak:24.0.2
+    env_file:
+      - .env
+    ports:
+      - '8082:8080'
+    command: [ "start-dev", "--import-realm", "--log-level=ERROR"]
+    volumes:
+      - ./config/keycloak/import/realm.json:/opt/keycloak/data/import/realm.json:ro
+    environment:
+      - KEYCLOAK_ADMIN=${KEYCLOAK_ADMIN}
+      - KEYCLOAK_ADMIN_PASSWORD=${KEYCLOAK_PASSWORD}
+      - KC_PROXY=edge
+      - KC_HOSTNAME_STRICT=true
+      - KC_HTTP_ENABLED=true
+      - KC_HTTPS_ENABLED=false
+      - KC_HOSTNAME=51.250.30.109
+    networks:
+      - app
+*/
