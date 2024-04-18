d6d60450d92342bdf04a8a4175a704575911b8eb
Gordey Dovydenko <inostaziss@gmail.com>
2024-04-18 20:09:52 +0700

    fix config keycloak

--- src/main/java/org/hits/backend/hackathon_tusur/HackathonTusurApplication.java
+++ src/main/java/org/hits/backend/hackathon_tusur/HackathonTusurApplication.java
@@ -36,7 +36,7 @@ https://stackoverflow.com/questions/38337895/globally-disable-https-keycloak
     networks:
       - app
 
-docker exec -it 9428a74644f4 bash
+docker exec -it 25c09dcefcad bash
 cd /opt/keycloak/bin/
 ./kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin
 ./kcadm.sh update realms/master -s sslRequired=NONE
