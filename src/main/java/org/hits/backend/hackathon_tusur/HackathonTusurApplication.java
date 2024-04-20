f66e348ec9ca4f6b9e24b3b6bff10889120fa458
Gordey Dovydenko <inostaziss@gmail.com>
2024-04-20 14:52:54 +0700

    delete entity

--- src/main/java/org/hits/backend/hackathon_tusur/HackathonTusurApplication.java
+++ src/main/java/org/hits/backend/hackathon_tusur/HackathonTusurApplication.java
@@ -36,7 +36,7 @@ https://stackoverflow.com/questions/38337895/globally-disable-https-keycloak
     networks:
       - app
 
-docker exec -it 07cd98f762cf bash
+docker exec -it 9556682afb1a bash
 cd /opt/keycloak/bin/
 ./kcadm.sh config credentials --server http://localhost:8080 --realm master --user admin
 ./kcadm.sh update realms/master -s sslRequired=NONE
